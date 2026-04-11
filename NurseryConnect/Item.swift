5ce61365ee9f0d8d28d59d7b9edda6999413cdc9
Arsh <arshartisan@gmail.com>
2026-04-11 13:09:05 +0530

    initial commit: new Swift project along with SwiftUI tests

---
+++ NurseryConnect/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  NurseryConnect
+//
+//  Created by FocalDive on 2026-04-11.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ NurseryConnectTests/NurseryConnectTests.swift
@@ -0,0 +1,17 @@
+//
+//  NurseryConnectTests.swift
+//  NurseryConnectTests
+//
+//  Created by FocalDive on 2026-04-11.
+//
+
+import Testing
+@testable import NurseryConnect
+
+struct NurseryConnectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
